abd594ce37a4d34799e18f4c11a053d0616fb613
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2019-11-27 20:15:49 +0200

    initial commit of morse code project

---
+++ MorseCode.java
@@ -0,0 +1,89 @@
+import java.util.HashMap;
+import java.util.Map;
+/* *********************************************** *
+ * @author : Ndumiso Onke Fanti                    *
+ *  Exercise : Morse Code                          *
+ * Description : This program is meant to take user*
+ * input as string and convert to morse code or    *
+ *  vice-versa                                     *
+ * *********************************************** */
+public class MorseCode {
+    public static void main(String[] args) {
+
+        /* ************************************************* *
+         *A Map best suitable to store keys and values.      *
+        * Keys will be characters and values will be their   *
+        * corresponding definition in Morse Code             *
+        * ************************************************** */
+        Map<Character, String> morseCode = new HashMap<>();
+        morseCode.put('A', ".-");
+        morseCode.put('B', "-...");
+        morseCode.put('C', "-.-.");
+        morseCode.put('D', "-..");
+        morseCode.put('E', ".");
+        morseCode.put('F', "..-.");
+        morseCode.put('G', "--.");
+        morseCode.put('H', "....");
+        morseCode.put('I', "..");
+        morseCode.put('J', ".---");
+        morseCode.put('K', "-.-");
+        morseCode.put('L', ".-..");
+        morseCode.put('M', "--");
+        morseCode.put('N', "-.");
+        morseCode.put('O', "---");
+        morseCode.put('P', ".--.");
+        morseCode.put('Q', "--.-");
+        morseCode.put('R', ".-.");
+        morseCode.put('S', "...");
+        morseCode.put('T', "-");
+        morseCode.put('U', "..-");
+        morseCode.put('V', "...-");
+        morseCode.put('W', ".--");
+        morseCode.put('X', "-..-");
+        morseCode.put('Y', "-.--");
+        morseCode.put('Z', "--..");
+        morseCode.put(' ', "/");
+
+        /* ********************* converting From : Letters TO: Morse Code************* */
+
+        char[] letters = lettersToMorseCode("Hi there");     // test case given in the problem descriptions
+
+        for (int i = 0; i < letters.length; i++) {
+            System.out.print(morseCode.get(letters[i]) + " ");
+        }
+        System.out.println();
+
+        /* ********************* converting From : Morse Code TO: Plain Text ************* */
+
+        /* ************************************************************ *
+         * Declare a map to store morse code as key                     *
+         *and characters as value for reverse conversion                *
+         * The same Map will be used to create the content of the new   *
+         * map, just values as key and keys and values this time around *
+         * ************************************************************ */
+        Map<String, Character> characters = new HashMap<>();
+
+        for (Map.Entry<Character,String> entry: morseCode.entrySet()){
+            char key = entry.getKey();
+            String value = entry.getValue();
+            characters.put(value,key);
+        }
+
+        String[] morseChars = morseCodeToLetters(".... .. / - .... . .-. . ");
+
+        for (int i = 0; i < morseChars.length; i++) {
+            if(morseChars[i].equals("")){
+                morseChars[i] = " ";
+            }
+            System.out.print(characters.get(morseChars[i]));
+        }
+        System.out.println();
+    }
+    private static char[] lettersToMorseCode(String words){
+
+        return words.toUpperCase().toCharArray();
+    }
+    private static String[] morseCodeToLetters(String morseword){
+        return morseword.split(" ");
+    }
+}
