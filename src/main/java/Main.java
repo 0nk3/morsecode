732e872e3251627c7c83d4056f6edf37b168850c
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2020-03-12 20:59:02 +0200

    Editting the main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,24 +14,32 @@ public class Main {
         // user options
         System.out.println("***********************************************************  *");
         System.out.println("*               SELECT 1 or 2                                *");
+        System.out.println("*        0. quit                                             *");
         System.out.println("*        1. From Plain Text To Morse Code                    * ");
         System.out.println("*        2. From Morse Code To Plain                         *");
         System.out.println("************************************************************ *");
 
-        System.out.println("SELECT : ");
-        option = input.nextLine();
+
         // processing
-        switch (option) {
-            case "1" :
-                System.out.println("Enter plain text : ");
-                plainTextSentence = input.nextLine();
-                morseCode.lettersToMorseCode(plainTextSentence);
-                break;
-            case "2" :
-                System.out.println("Enter morse code : ");
-                morseCodeSentence = input.nextLine();
-                morseCode.morseCodeToLetters(morseCodeSentence);
-                break;
-        }
+        do {
+            System.out.println("SELECT(0 or 1 or 2) : ");
+            option = input.nextLine();
+
+            switch (option) {
+                case "1" :
+                    System.out.println("Enter plain text : ");
+                    plainTextSentence = input.nextLine();
+                    morseCode.lettersToMorseCode(plainTextSentence);
+                    break;
+                case "2" :
+                    System.out.println("Enter morse code : ");
+                    morseCodeSentence = input.nextLine();
+                    morseCode.morseCodeToLetters(morseCodeSentence);
+                    break;
+            }
+            System.out.println();
+        }while (!option.equals("0"));
+        System.out.println("<========================== Goodbye ====================================> ");
+
     }
 }
